e7b911e7693884bfa0c93e726ca6621a07377deb
Brandon Bohling <bohling@gmail.com>
2012-12-14 22:35:44 -0800

    Initial commit

---
+++ content-link.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The template for displaying posts in the Link post format
+ *
+ * @package CrackerJack
+ * @since CrackerJack 1.0
+ */
+?>
+
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<header><?php _e( 'Link', 'cJack' ); ?></header>
+		<div class="entry-content">
+			<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'cJack' ) ); ?>
+		</div><!-- .entry-content -->
+
+		<footer class="entry-meta">
+			<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'cJack' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php echo get_the_date(); ?></a>
+			<?php if ( comments_open() ) : ?>
+			<div class="comments-link">
+				<?php comments_popup_link( '<span class="leave-reply">' . __( 'Leave a reply', 'cJack' ) . '</span>', __( '1 Reply', 'cJack' ), __( '% Replies', 'cJack' ) ); ?>
+			</div><!-- .comments-link -->
+			<?php endif; // comments_open() ?>
+			<?php edit_post_link( __( 'Edit', 'cJack' ), '<span class="edit-link">', '</span>' ); ?>
+		</footer><!-- .entry-meta -->
+	</article><!-- #post -->
---
+++ header.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * The Header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="main">
+ *
+ * @package CrackerJack
+ * @since CrackerJack 1.0
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width" />
+<title><?php wp_title( '|', true, 'right' ); ?></title>
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<!--[if lt IE 9]>
+<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
+<![endif]-->
+
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="hfeed site">
+	<?php do_action( 'before' ); ?>
+	<header id="masthead" class="site-header" role="banner">
+		<hgroup>
+			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+		</hgroup>
+
+		<nav role="navigation" class="site-navigation main-navigation">
+			<h1 class="assistive-text"><?php _e( 'Menu', 'cJack' ); ?></h1>
+			<div class="assistive-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'cJack' ); ?>"><?php _e( 'Skip to content', 'cJack' ); ?></a></div>
+
+			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+		</nav><!-- .site-navigation .main-navigation -->
+	</header><!-- #masthead .site-header -->
+
+	<div id="sidebar">
+		<div id="sidebar-icons">
+			<ul>
+				<li><img src="<?php echo get_bloginfo('template_directory'); ?>/images/06-magnify@2x.png" /></li>
+				<li><img src="<?php echo get_bloginfo('template_directory'); ?>/images/12-eye@2x.png" /></li>
+			</ul>
+		</div>
+	
+	</div>
+
+	<div id="main" class="site-main">
